2aecab0bd0d53d9a912daf66f5830196774a54ee
KDJ1221 <kimmikojames@gmail.com>
2020-08-11 07:24:42 -0700

    Post entries to the page

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // javascript
+
+var entryForm = document.getElementById("entryForm");
+var entriesSection = document.getElementById("entries");
+var entryTextbox = document.getElementsByClassName('entry-textbox');
+
+function addEntry(event){
+    event.preventDefault();
+    var entryDiv = document.createElement("div");
+    entryDiv.className = "single-entry";
+    entryDiv.innerText = entryTextbox[0].value;
+    entriesSection.appendChild(entryDiv);
+}
+
+entryForm.addEventListener("submit", addEntry);
