54e87e92870d143e17639623c2c2ceda6e93008a
KDJ1221 <kimmikojames@gmail.com>
2020-08-13 08:13:31 -0700

    Adding more JS

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 // javascript
+//can use querySelector to get both classes & ids
+var entryForm = document.getElementById('entryForm');
+var entriesSection = document.querySelector("#entries");
+var entryTextbox = document.querySelector('.entry-textbox');
+var entriesNav = document.querySelector('.entries-nav');
 
-var entryForm = document.getElementById("entryForm");
-var entriesSection = document.getElementById("entries");
-var entryTextbox = document.getElementsByClassName('entry-textbox');
-
+count = 1;
 function addEntry(event){
     event.preventDefault();
-    var entryDiv = document.createElement("div");
-    entryDiv.className = "single-entry";
-    entryDiv.innerText = entryTextbox[0].value;
+    var entryDiv = document.createElement('div');
+    entryDiv.className = 'single-entry';
+    entryDiv.innerText = entryTextbox.value;
+    entryDiv.style.display = 'none';
     entriesSection.appendChild(entryDiv);
-    entryTextbox[0].value = "";
+    entryTextbox.value = '';
+
+    var displayEntryButton = document.createElement('button');
+    displayEntryButton.className = 'display-entry-button';
+    displayEntryButton.innerText = count;
+    entriesNav.appendChild(displayEntryButton);
+    
+    displayEntryButton.addEventListener('click', function(){
+        const allEntries = document.querySelectorAll('.single-entry');
+        for(var i = 0; i < allEntries.length; i++){
+            allEntries[i].style.display = 'none';
+        }
+
+        entryDiv.style.display = 'block';
+        
+    })
+
+    count+=1
+
 }
 
-entryForm.addEventListener("submit", addEntry);
+entryForm.addEventListener('submit', addEntry);
